d4522fc87883cc8e3ed5db05b8ea5c19b7946c59
theogf <theo.galyfajou@gmail.com>
2024-07-20 19:45:50 +0200

    Some testing functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,27 @@ using Aqua
     @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(Troubadour)
     end
+    @play_midi sqrt(2)
     # Write your tests here.
 end
+
+function f()
+    s = 0
+    for val in x
+        s = s + val
+    end
+    return s
+end
+x = Any[0.4, 1, true, 0.3f2, 0x12]
+
+function f2(x)
+    s = zero(eltype(x))
+    for val in x
+        s += val
+    end
+    return s
+end
+y = rand(100)
+
+@llvm_midi f()
+@llvm_midi f2(y)
